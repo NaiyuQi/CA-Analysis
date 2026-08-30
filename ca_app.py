2c764bc0a271f81d3a722a6caa2dd425d1e7859a
NaiyuQi <65248919+NaiyuQi@users.noreply.github.com>
2026-08-30 22:08:08 +0200

    Implement download button for cycle overview image
    Add download button for overview image of cycles

--- ca_app.py
+++ ca_app.py
@@ -92,6 +92,7 @@ if uploaded_file:
             fig.add_trace(go.Scatter(x=s2['pos2'], y=s2['wt2'], mode='lines',
                                      name=f'Cycle {c} Rec', line=dict(color=colors_rec[c-1], dash='dash')))
         st.plotly_chart(fig, width='stretch')
+        
         img_bytes = pio.to_image(fig, format='png', width=900, height=400)
             st.download_button(label="Download Overview", data=img_bytes,
                    file_name=f"{base_name}_all_cycles.png", mime="image/png")
