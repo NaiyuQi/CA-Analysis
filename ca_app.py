a86647219afb8b683d5412acf9d1d1d8d42a1101
NaiyuQi <65248919+NaiyuQi@users.noreply.github.com>
2026-09-06 23:33:12 +0200

    Update ca_app.py

--- ca_app.py
+++ ca_app.py
@@ -446,3 +446,7 @@ if uploaded_file:
                 | x2 | {x2:.6f} |
                 | x3 | {x3:.6f} |
                 """)
+
+st.write(f"set1 x: {set1_nd['x1_nd'].min():.4f} to {set1_nd['x1_nd'].max():.4f}")
+st.write(f"set2 x: {set2_nd['x2_nd'].min():.4f} to {set2_nd['x2_nd'].max():.4f}")
+st.write(f"Common x: {x_min_c:.4f} to {x_max_c:.4f}")
